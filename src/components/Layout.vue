972f22658358b4b1b17882a9f1b14c867f3aef20
Michael Cruz <michael.cruz@uwalumni.com>
2017-09-30 18:25:24 -0700

    modified main.js

---
+++ src/components/Layout.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+
+  <div class="hello">
+    <h1>Hello, world.</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+import { store } from './store/store';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  // works in developmemt mode with webpack, but the server will need to be configured to return all cases
+  mode: 'history'
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  store,
+  render: h => h(App)
 })
