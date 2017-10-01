4fd0479b15efb2df090ad0b5d38064e29757a5c2
Michael Cruz <michael.cruz@uwalumni.com>
2017-09-30 20:25:42 -0700

    set up for deployment

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -31,7 +31,37 @@ export default {
           "Content-Type": "application/json"
         },
         mode: 'cors',
-        body: '{"on":true, "sat":254, "bri":254,"hue":12750}'
+        // body: '{"on":true, "sat":254, "bri":254,"hue":25500}'
+        body: '{"effect":"none"}'
+      })
+        .then(
+          function(response) {
+            console.log(response);
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+              return;
+            }
+            // Examine the text in the response
+            response.json().then(function(data) {
+              console.log('cool', data);
+            });
+          }
+        )
+        .catch(function(err) {
+          console.log('Fetch Error :-S', err);
+        });
+    },
+    setGroup() {
+      let _self = this;
+      fetch(huerl + '/groups/1/action', {
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        mode: 'cors',
+        body: '{"on": true,"hue": 12750}'
+        // body: '{"effect":"none"}'
       })
         .then(
           function(response) {
@@ -53,7 +83,7 @@ export default {
     }
   },
   beforeMount() {
-    this.fetch();
+    // this.setGroup();
   }
 }
 </script>
